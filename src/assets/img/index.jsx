19eec477357fcdde6e4d14820eb003f247b35933
Vadim Rysev <rysev@goout.net>
2023-12-18 19:58:55 +0100

    added new component for choosing category - Categories

---
+++ src/assets/img/index.jsx
@@ -0,0 +1,4 @@
+export { default as fries } from "./fries.svg";
+export { default as burger } from "./sandwich.svg";
+export { default as pack } from "./pack.svg";
+export { default as pizza } from "./pizza.svg";

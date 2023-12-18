bbdd3afec0890eb47c2e83c047fe13d7e42266ab
Vadim Rysev <rysev@goout.net>
2023-12-18 05:26:41 +0100

    added and implemented new component - hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
 import "./scss/app.scss";
 import Header from "./components/Header/Header.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Hero from "./components/Hero/Hero.jsx";
 function App() {
   return (
     <>
       <Navbar />
       <div className="wrapper">
         <Header />
-        <div className="hero">
-          <div className="container">
-            <div className="hero__section">
-              <div className="hero__block">
-                <div className="hero__block--title">
-                  Make Your First Order and Get
-                </div>
-                <div className="hero__block--text">
-                  In publishing and graphic design, Lorem ipsum is a placeholder
-                  text commonly used to demonstrate the visual form of a
-                  document or a typeface without.
-                </div>
-                <div className="hero__block--button">Order Now</div>
-              </div>
-              <div className="hero__section--img"></div>
-            </div>
-          </div>
-        </div>
+        <Hero />
       </div>
     </>
   );

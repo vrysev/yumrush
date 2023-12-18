5f320b6b0481ade26a8c06c1dcadc3370f33d5d0
Vadim Rysev <rysev@goout.net>
2023-12-18 02:31:51 +0100

    added new component - navigation side bar + toggle button

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,8 @@
+export { default as balance } from "./balance.svg";
+export { default as chat } from "./chat.svg";
+export { default as email } from "./email.svg";
+export { default as menu } from "./menu.svg";
+export { default as person } from "./person.svg";
+export { default as piechart } from "./piechart.svg";
+export { default as settings } from "./settings.svg";
+export { default as expand } from "./expand.svg";

b90df075dde044c0a2e1e0adb5323a55b830827b
Vadim Rysev <rysev@goout.net>
2023-12-25 08:19:05 +0100

    added react router and some basic routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
 import "./scss/app.scss";
-import Header from "./components/Header/Header.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Hero from "./components/Hero/Hero.jsx";
-import Categories from "./components/Categories/Categories.jsx";
-import Product from "./components/Product/Product.jsx";
-import SkeletonProduct from "./components/Product/SkeletonProduct.jsx";
-import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
 function App() {
-  const [pizzas, setPizzas] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    fetch("https://65810e6e3dfdd1b11c425bad.mockapi.io/pizzas")
-      .then((res) => res.json())
-      .then((data) => {
-        setPizzas(data);
-        setIsLoading(false);
-      });
-  }, []);
-
   return (
     <>
       <Navbar />
       <div className="wrapper">
-        <Header />
-        <Hero />
-        <Categories />
-        <div className="products">
-          <div className="container">
-            <div className="products__section">
-              {isLoading
-                ? [...new Array(6)].map((_, index) => (
-                    <SkeletonProduct key={index} />
-                  ))
-                : pizzas.map((obj) => <Product key={obj.id} {...obj} />)}
-            </div>
-          </div>
-        </div>
+        <Outlet />
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <div>ERROR</div>,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "yes",
+        element: <div>YES</div>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);

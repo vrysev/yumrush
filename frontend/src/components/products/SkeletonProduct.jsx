35d561ab3c3441a691dc165e5175431cec08eb22
Vadim Rysev <rysev@goout.net>
2025-03-04 08:26:54 +0100

    updated skeletons-placeholders for products

--- frontend/src/components/products/SkeletonProduct.jsx
+++ frontend/src/components/products/SkeletonProduct.jsx
@@ -3,21 +3,21 @@ import ContentLoader from 'react-content-loader';
 const SkeletonProduct = (props) => {
   return (
     <div className="product product--skeleton">
-      <ContentLoader
-        speed={2}
-        width={350}
-        height={160}
-        viewBox="0 0 350 160"
-        backgroundColor="#f3f3f3"
-        foregroundColor="#ecebeb"
-        {...props}
-      >
-        <circle cx="80" cy="80" r="60" />
-        <rect x="160" y="30" rx="4" ry="4" width="150" height="20" />
-        <rect x="160" y="70" rx="3" ry="3" width="100" height="15" />
-        <rect x="160" y="110" rx="3" ry="3" width="70" height="20" />
-        <rect x="245" y="110" rx="8" ry="8" width="80" height="25" />
-      </ContentLoader>
+        <ContentLoader 
+    speed={1}
+    width={300}
+    height={437}
+    viewBox="0 0 300 437"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="142" cy="164" r="116" /> 
+    <rect x="35" y="322" rx="0" ry="0" width="224" height="18" /> 
+    <rect x="35" y="348" rx="0" ry="0" width="132" height="11" /> 
+    <rect x="35" y="375" rx="0" ry="0" width="63" height="15" /> 
+    <rect x="193" y="365" rx="0" ry="0" width="63" height="27" />
+  </ContentLoader>
     </div>
   );
 };
